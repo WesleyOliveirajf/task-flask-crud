71198d188b0b7866ac6e0e596c465175e482bc52
Wesley Oliveira <lojastioinfo@gmail.com>
2025-10-25 17:32:32 -0300

    aula flask api
    aula flask api

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+from models.task import Task
 
 app = Flask(__name__)
 
+# CRUD
+# Create, Read, Update and Delete = Criar, Ler, Atualizar e Deletar
+# Tabela: Tarefa
 
-if __name__ == "__main__":
-    app.run(debug=True) #typr
+tasks = []
 
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    data = request.get_json()
+    print(data)
+    return'Test'
 
-
+if __name__ == '__main__':
+    app.run(debug=True) # aula CRUD 2 - 
